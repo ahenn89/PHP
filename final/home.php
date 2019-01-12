ffd41287440fa59aca217800a5206acdac19785c
ahenn89 <31427701+ahenn89@users.noreply.github.com>
2019-01-12 09:18:55 -0600

    Add files via upload

---
+++ final/home.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang='eng'>
+
+<head>
+   <meta charset='utf-8'>
+   <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+   <link rel="stylesheet" type="text/css" href="styles.css" media="screen" />
+   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+   <script src="script.js"></script>
+   <title>Wedding Website</title>
+</head>
+
+<body>
+
+    <header>
+        <img src="images/i-do-we-do-logo.jpg" />
+    </header>
+    
+    <div id='cssmenu'>
+        <ul>
+           <li><a href='home.php'><span>Home</span></a></li>
+           <li><a href='food.php'><span>Food Choices</span></a></li>
+           <li><a href='hotel.php'><span>Hotel Search</span></a></li>
+           <li><a href='contact.php'><span>Contact</span></a></li>
+           <li class='last'><a href='backend.php'><span>Backend Login</span></a></li>
+        </ul>
+    </div>
+	
+    <div id='article'>
+    	<img src="images/like3.jpg" />
+    	<p class='article'>We met at OfficeMax. I know, not very glamorous or romantic. We didn't even start dating till almost a full year later but once we did the rest they say is history. Please take some time to explore our site. We are looking for some specific information from you, so if you could fill them out as you come across them that would be awesome.</p>
+    </div>
+	
+	<aside>
+    	<h2>We Are Registered At These Stores</h2>
+        	<div id='kohls'>
+        		<img src="images/Kohls-CouponSign-up-now-and-get-5-off.png" />
+            </div>
+            <div id='crate'>
+            	<img src="images/crate-a.jpg" />
+            </div>
+    </aside>
+
+<footer>
+</footer>
+</body>
+<html>
